7fe4764114c57a0369da8c150b65c42b851f2379
Mihir Jethwa <mihirjethwa7@gmail.com>
2021-02-26 23:23:26 +0530

    forth commit

--- server.js
+++ server.js
@@ -54,16 +54,16 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.sendFile(__dirname, "client", "build", "index.html"));
-  // });
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.sendFile(__dirname, "client", "build", "index.html"));
   });
+  // app.use(express.static(path.join(__dirname, "./client/build")));
+
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  // });
 }
 
 //Creating port
