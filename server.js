9c4eb5d0c8ab6cdf479e53afe1e6a522e3cbae3d
Mihir Jethwa <mihirjethwa7@gmail.com>
2021-02-26 22:07:24 +0530

    third commit

--- server.js
+++ server.js
@@ -56,10 +56,11 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV === "production") {
   //app.use(express.static("client/build"));
   //app.use(express.static(path.join(__dirname, "client", "build", "index.html")));
-  app.use(express.static(path.join(__dirname, "../build")));
+  // app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
 
