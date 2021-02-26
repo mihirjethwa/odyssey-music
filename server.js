952dfbec935920f90e8c8b43db4e45f3691b44dd
Mihir Jethwa <mihirjethwa7@gmail.com>
2021-02-27 01:39:54 +0530

    forth commit

--- server.js
+++ server.js
@@ -56,9 +56,9 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.sendFile(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.sendFile(__dirname, "client", "build", "index.html"));
+  // });
   // app.use(express.static(path.join(__dirname, "./client/build")));
 
   // app.get("*", (req, res) => {
